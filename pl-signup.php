9c1b5a058a448b9c5f6992f7c33f31d68317fa4b
Simon Prosser <simon@pagelines.com>
2014-07-03 20:31:41 +0100

    fix default and remove validation error

--- pl-signup.php
+++ pl-signup.php
@@ -19,18 +19,18 @@ if( ! class_exists( 'PageLinesSection' ) )
 class DMS_Signup extends PageLinesSection{
 
 	function section_styles(){
-		
+
 		$ajax_url = admin_url( 'admin-ajax.php' );
 		wp_enqueue_script( 'jquery-ui-draggable' );
 		wp_enqueue_script( 'dms-signup', $this->base_url.'/scripts.js', array( 'jquery' ), pl_get_cache_key(), false );
 		wp_enqueue_script( 'dms-signup-fittext', $this->base_url.'/jquery.fittext.js', array( 'jquery' ), '1.2', false );
-		
+
 
 		if ( has_action( 'pl_force_ssl' ) )
 			$ajax_url = str_replace( 'http://', 'https://', $ajax_url );
-		
-		
-		
+
+
+
 		wp_localize_script( 'dms-signup', 'dmssignupajax', array(
 			'url'			=> $ajax_url,
 			'replytxt'		=> pl_setting( 'dms_signup_success_txt', array( 'default' => "[email] was subscribed <i class='icon icon-smile'></i>" ) ),
@@ -70,8 +70,8 @@ class DMS_Signup extends PageLinesSection{
 					array(
 						'key'		=> 'dms_signup_submit',
 						'type'		=> 'text',
-						'default'	=> 'Submit <i class="icon icon-chevron-sign-right"></i>',
-						'place'		=> 'Submit <i class="icon icon-chevron-sign-right"></i>',
+						'default'	=> "Submit <i class='icon icon-chevron-sign-right'></i>",
+						'place'		=> "Submit <i class='icon icon-chevron-sign-right'></i>",
 						'label'		=> 'Submit button text',
 					),
 					array(
@@ -125,17 +125,17 @@ class DMS_Signup extends PageLinesSection{
 
 	function section_template() {
 		$send_btn_text = $this->opt( 'dms_signup_submit', array( 'default' => 'Submit <i class="icon icon-chevron-sign-right"></i>' ) );
-		$subscribe_btn_text = $this->opt( 'dms_signup_subscribe', array( 'default' => 'Subscribe' ) );
+		$subscribe_btn_text = $this->opt( 'dms_signup_subscribe', array( 'default' => 'Email Subscribe' ) );
 		$subscribe_btn_theme = $this->opt( 'dms_submit_btn_theme', array( 'default' => 'btn-primary') );
 		?>
 		<div class="dms-signup dms-signup-light">
-			<div class="drag-slider pl-animation-group" style="display: none;">				
+			<div class="drag-slider pl-animation-group" style="display: none;">
 				<div class="drag-well pl-animation pla-from-bottom"><h2><strong id="slider" class="the-slider"><i class="icon icon-long-arrow-right"></i></strong> <?php echo $subscribe_btn_text; ?></h2></div>
 			</div>
 			<div class="reply-text" style="display: none;">
 				<div class="saving"></div>
 				<div class="first-steps">
-					<span class="pl-signup-form the-inputs pl-animation-group" action="#">
+					<span class="pl-signup-form the-inputs pl-animation-group">
 						<input type="email" class="pl-animation pla-from-bottom get-pl-email the-email" value="" placeholder="Email Address" />
 						<span class="btn <?php echo $subscribe_btn_theme;?> btn-large get-pagelines pl-send-email pl-animation pla-from-bottom">
 							<span class="get-pl-text" data-sync="get_pagelines_btn_text"><?php echo $send_btn_text; ?></span>
